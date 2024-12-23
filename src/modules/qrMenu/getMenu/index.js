c655cb3bc18fed273c9d65cbd5307f7aa338e017
enteresanlikk <zeki1198@gmail.com>
2024-12-24 02:26:40 +0300

    Add branches to qr menu route.

--- src/modules/qrMenu/getMenu/index.js
+++ src/modules/qrMenu/getMenu/index.js
@@ -1,14 +1,27 @@
 const Tenant = require('../../tenant/tenant.model');
+const TenantBranch = require('../../tenantBranch/tenantBranch.model');
 
 module.exports = async (request, reply) => {
     const tenantId = request.tenant._id;
 
-    const tenant = await Tenant
-        .findOne({
-            _id: tenantId,
-        }, '-qrCode -users')
-        .populate('languages.language')
-        .populate('currencies.currency');
+    let [tenant, tenantBranches] = await Promise.all([
+        await Tenant
+            .findOne({
+                _id: tenantId,
+            }, '-qrCode -users')
+            .populate('languages.language')
+            .populate('currencies.currency'),
+        await TenantBranch
+            .find({
+                tenant: tenantId,
+            })
+            .populate('translations.language')
+    ]);
+
+    if (tenant) {
+        tenant = tenant.toObject();
+        tenant.branches = tenantBranches;
+    }
 
     return reply.send({
         success: true,
