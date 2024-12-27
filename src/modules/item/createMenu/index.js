67af35b6c77f0bee55a2b0c0a36bfa7e16b706a3
enteresanlikk <zeki1198@gmail.com>
2024-12-28 00:34:59 +0300

    Update create menu with recursive.

--- src/modules/item/createMenu/index.js
+++ src/modules/item/createMenu/index.js
@@ -2,6 +2,30 @@ const Item = require('../item.model');
 const ItemRelation = require('../../itemRelation/itemRelation.model');
 const { mongoose } = require('@lumodine/mongodb');
 
+async function createItemRecursively({ tenantId, item, sort, session }) {
+    const itemDoc = await Item.create([{
+        tenant: tenantId,
+        kind: item.kind,
+        translations: item.translations || undefined,
+        prices: item.prices || undefined,
+        isShowInMenu: true,
+        sort: sort,
+    }], { session });
+
+    if (item.items && item.items.length > 0) {
+        for (let i = 0; i < item.items.length; i++) {
+            const childDoc = await createItemRecursively({ tenantId, item: item.items[i], sort: i, session });
+
+            await ItemRelation.create([{
+                sourceItem: itemDoc[0]._id,
+                targetItem: childDoc[0]._id,
+            }], { session });
+        }
+    }
+
+    return itemDoc;
+}
+
 module.exports = async (request, reply) => {
     const tenantId = request.tenant._id;
     const { items } = request.body;
@@ -10,58 +34,8 @@ module.exports = async (request, reply) => {
     session.startTransaction();
 
     try {
-        let itemIndex = 0;
-        for (const item of items) {
-            const itemDoc = await Item.create([{
-                tenant: tenantId,
-                kind: item.kind,
-                translations: item.translations,
-                prices: item.prices || undefined,
-                isShowInMenu: true,
-                sort: itemIndex,
-            }], { session });
-
-            let subItemIndex = 0;
-            for (const subItem of item.items) {
-                const subItemDoc = await Item.create([{
-                    tenant: tenantId,
-                    kind: subItem.kind,
-                    translations: subItem.translations,
-                    prices: subItem.prices || undefined,
-                    isShowInMenu: true,
-                    sort: subItemIndex,
-                }], { session });
-
-                await ItemRelation.create([{
-                    sourceItem: itemDoc[0]._id,
-                    targetItem: subItemDoc[0]._id
-                }], { session });
-
-                if (subItem.variants && subItem.variants.length > 0) {
-                    let variantIndex = 0;
-                    for (const variant of subItem.variants) {
-                        const variantDoc = await Item.create([{
-                            tenant: tenantId,
-                            kind: variant.kind,
-                            translations: variant.translations,
-                            prices: variant.prices || undefined,
-                            isShowInMenu: true,
-                            sort: variantIndex,
-                        }], { session });
-
-                        await ItemRelation.create([{
-                            sourceItem: subItemDoc[0]._id,
-                            targetItem: variantDoc[0]._id
-                        }], { session });
-
-                        variantIndex++;
-                    }
-                }
-
-                subItemIndex++;
-            }
-
-            itemIndex++;
+        for (let i = 0; i < items.length; i++) {
+            await createItemRecursively({ tenantId, item: items[i], sort: i, session });
         }
 
         await session.commitTransaction();
