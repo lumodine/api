662a7b8698c26744d4f03463603571a1d2f0bf0a
enteresanlikk <zeki1198@gmail.com>
2025-01-03 01:20:42 +0300

    Fix update product variant.

--- src/features/productVariant/updateProductVariant/index.js
+++ src/features/productVariant/updateProductVariant/index.js
@@ -1,16 +1,14 @@
 const ProductVariant = require('../productVariant.model');
-const ItemRelation = require('../../itemRelation/itemRelation.model');
-const { ITEM_KINDS } = require('../../item/item.constant');
 
 module.exports = async (request, reply) => {
     const {
         tenantId,
         productVariantId,
     } = request.params;
+
     const {
         translations,
         prices,
-        productId,
     } = request.body;
 
     const productVariant = await ProductVariant
@@ -41,24 +39,6 @@ module.exports = async (request, reply) => {
         return reply.send({
             success: false,
             message: request.i18n.product_variant_update_error,
-        });
-    }
-
-    if (productId) {
-        await ItemRelation.deleteMany({
-            'source.item': productId,
-            'target.item': productVariantId
-        });
-
-        await ItemRelation.create({
-            source: {
-                item: productId,
-                kind: ITEM_KINDS.PRODUCT,
-            },
-            target: {
-                item: productVariantId,
-                kind: ITEM_KINDS.PRODUCT_VARIANT,
-            }
         });
     }
 
