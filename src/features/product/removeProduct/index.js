4a064dfb05593abfdb60e2ae8fc825960ffc2066
enteresanlikk <zeki1198@gmail.com>
2025-01-03 15:45:40 +0300

    Fix remove product and variant logic.

--- src/features/product/removeProduct/index.js
+++ src/features/product/removeProduct/index.js
@@ -1,4 +1,6 @@
 const Product = require('../product.model');
+const ItemRelation = require('../../itemRelation/itemRelation.model');
+const { mongoose } = require('@lumodine/mongodb');
 
 module.exports = async (request, reply) => {
     const {
@@ -19,16 +21,47 @@ module.exports = async (request, reply) => {
         });
     }
 
-    const isRemoved = await Product.findByIdAndDelete(product._id);
-    if (!isRemoved) {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const isRemoved = await Product.findByIdAndDelete(product._id, { session });
+
+        const [
+            isRemovedRelations,
+        ] = await Promise.all([
+            ItemRelation.deleteMany({
+                $or: [
+                    { 'source.item': product._id },
+                    { 'target.item': product._id }
+                ]
+            }, { session }),
+        ]);
+
+        if (!isRemoved || !isRemovedRelations) {
+            await session.abortTransaction();
+            session.endSession();
+
+            return reply.send({
+                success: false,
+                message: request.i18n.product_remove_error,
+            });
+        }
+
+        await session.commitTransaction();
+        session.endSession();
+
+        return reply.send({
+            success: true,
+            message: request.i18n.product_remove_success,
+        });
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+
         return reply.send({
             success: false,
             message: request.i18n.product_remove_error,
         });
     }
-
-    return reply.send({
-        success: true,
-        message: request.i18n.product_remove_success,
-    });
 };
--- src/features/productVariant/removeProductVariant/index.js
+++ src/features/productVariant/removeProductVariant/index.js
@@ -1,4 +1,6 @@
 const ProductVariant = require('../productVariant.model');
+const ItemRelation = require('../../itemRelation/itemRelation.model');
+const { mongoose } = require('@lumodine/mongodb');
 
 module.exports = async (request, reply) => {
     const {
@@ -6,29 +8,60 @@ module.exports = async (request, reply) => {
         productVariantId,
     } = request.params;
 
-    const product = await ProductVariant
+    const productVariant = await ProductVariant
         .findOne({
             tenant: tenantId,
             _id: productVariantId,
         });
 
-    if (!product) {
+    if (!productVariant) {
         return reply.send({
             success: false,
             message: request.i18n.product_variant_not_found,
         });
     }
 
-    const isRemoved = await Product.findByIdAndDelete(product._id);
-    if (!isRemoved) {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const isRemoved = await ProductVariant.findByIdAndDelete(productVariant._id, { session });
+
+        const [
+            isRemovedRelations,
+        ] = await Promise.all([
+            ItemRelation.deleteMany({
+                $or: [
+                    { 'source.item': productVariant._id },
+                    { 'target.item': productVariant._id }
+                ]
+            }, { session }),
+        ]);
+
+        if (!isRemoved || !isRemovedRelations) {
+            await session.abortTransaction();
+            session.endSession();
+
+            return reply.send({
+                success: false,
+                message: request.i18n.product_variant_remove_error,
+            });
+        }
+
+        await session.commitTransaction();
+        session.endSession();
+
+        return reply.send({
+            success: true,
+            message: request.i18n.product_variant_remove_success,
+        });
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+
         return reply.send({
             success: false,
             message: request.i18n.product_variant_remove_error,
         });
     }
-
-    return reply.send({
-        success: true,
-        message: request.i18n.product_variant_remove_success,
-    });
 };
